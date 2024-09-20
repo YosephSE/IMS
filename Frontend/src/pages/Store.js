4183dbce5d8ce381c8ac38dda9174e0a65d0ad76
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-09-20 20:58:13 +0300

    store url added

--- Frontend/src/pages/Store.js
+++ Frontend/src/pages/Store.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import AddStore from "../components/AddStore";
 import AuthContext from "../AuthContext";
+import { api } from "../api";
 
 function Store() {
   const [showModal, setShowModal] = useState(false);
@@ -14,7 +15,7 @@ function Store() {
 
   // Fetching all stores data
   const fetchData = () => {
-    fetch(`http://localhost:4000/api/store/get/${authContext.user}`)
+    fetch(`${api}/store/get/${authContext.user}`)
       .then((response) => response.json())
       .then((data) => {
         setAllStores(data);
