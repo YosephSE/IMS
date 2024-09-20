9bae11630ddf61ccf9f4501a7ebbf35a1b9baf24
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-09-20 20:48:37 +0300

    login page using env api

--- Frontend/src/pages/Login.js
+++ Frontend/src/pages/Login.js
@@ -2,6 +2,7 @@
 import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import AuthContext from "../AuthContext";
+import { api } from "../api";
 
 function Login() {
   const [form, setForm] = useState({
@@ -19,7 +20,7 @@ function Login() {
 
   const authCheck = () => {
     setTimeout(() => {
-      fetch("http://localhost:4000/api/login")
+      fetch(`${api}/login`)
         .then((response) => response.json())
         .then((data) => {
           alert("Successfully Login");
@@ -40,7 +41,7 @@ function Login() {
     if (form.email === "" || form.password === "") {
       alert("To login user, enter details to proceed...");
     } else {
-      fetch("http://localhost:4000/api/login", {
+      fetch(`${api}/login`, {
         method: "POST",
         headers: {
           "Content-type": "application/json",
