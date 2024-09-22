5e206e05063362d8117afd82ed6d7929236f7da1
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-09-23 00:28:19 +0300

    toaster updated

--- Frontend/src/components/toast.jsx
+++ Frontend/src/components/toast.jsx
@@ -1,7 +1,7 @@
-import toast, { Toaster } from "react-hot-toast";
+import toast from "react-hot-toast";
 
 const successToast = (message) => toast.success(message);
 
 const errorToast = (message) => toast.error(message);
 
-export { successToast, errorToast, Toaster };
+export { successToast, errorToast };
